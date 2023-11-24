7eefa49763d4b20380cbcab67b41fdb78b3215ca
ShuvoBaroi-DesignManiaBD <contact.shuvobaroi@gmail.com>
2023-11-24 22:47:02 +0600

    Addition: Firebase connected succesfully

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,8 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
+import Header from "../Pages/Shared/Header/Header";
 
 const MainLayout = () => {
     return (
+        <>
+        <Header></Header>
         <Outlet></Outlet>
+        <Footer></Footer>
+        </>
     );
 };
 
